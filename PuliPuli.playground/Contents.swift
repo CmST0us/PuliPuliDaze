17a00115440f523a9aa87eb18fdb219664d29607
eric3u <eric3u@outlook.com>
2019-04-07 23:42:23 +0800

    [Refactor] PuliPuliDownloader 分离

--- PuliPuli.playground/Contents.swift
+++ PuliPuli.playground/Contents.swift
@@ -3,62 +3,7 @@ import CommonCrypto
 
 Env.checkAndCreateDownloadDocument()
 
-class PuliPuliDownloader: NSObject, VideoDownloaderDelegate {
-    
-    
-    public var downloadItem: [(Int, VideoQuality)] = []
-    public var maxConcurrentOperationCount: Int {
-        set {
-            self.downloader.maxConcurrentOperationCount = newValue
-        }
-        
-        get {
-            return self.downloader.maxConcurrentOperationCount
-        }
-    }
-    
-    var videoInfo: VideoInfo!
-    var avNumber: Int = 0
-    
-    lazy var downloader: VideoDownloader = {
-        let d = VideoDownloader()
-        d.delegate = self
-        return d
-    }()
-    
-    public init(av: Int) {
-        self.avNumber = av
-    }
-    
-    public func work() {
-        self.downloader.start()
-    }
-    
-    public func fetchVideoInfo() {
-        let videoInfo = VideoInfo(av: self.avNumber)
-        let (ret, _) = videoInfo.fetchInfo()
-        if (ret) {
-            print("Video \(videoInfo.title) Has Pages:")
-            videoInfo.pagesTitle.enumerated().forEach { (i) in
-                print("     \(i.offset): \(i.element)")
-            }
-            self.videoInfo = videoInfo
-        }
-    }
-    
-    func downloader(_ downloader: VideoDownloader, didTriggerEvent: VideoDownloaderEvent) {
-        if (didTriggerEvent == .CanAddTask) {
-            if (self.downloadItem.count > 0) {
-                let item = self.downloadItem.removeFirst()
-                let page = VideoPageInfo(videoItem: self.videoInfo.videoPages[item.0])
-                page.fetchDownloadURL(quality: item.1)
-                downloader.addTask(url: page.downloadURL[0], videoItem: page.videoItem)
-            }
-        }
-    }
-}
-
-let pulipuli = PuliPuliDownloader(av: 46436988)
+let pulipuli = PuliPuliDownloader(av: 47896059)
 pulipuli.fetchVideoInfo()
 var s: [(Int, VideoQuality)] = []
 for i in 0...1 {
@@ -68,5 +13,6 @@ for i in 0...1 {
 pulipuli.maxConcurrentOperationCount = 1
 pulipuli.downloadItem = s
 pulipuli.work()
+
 
 
---
+++ PuliPuli.playground/Sources/PuliPuliDownloader.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+public class PuliPuliDownloader: NSObject, VideoDownloaderDelegate {
+    
+    public var downloadItem: [(Int, VideoQuality)] = []
+    public var maxConcurrentOperationCount: Int {
+        set {
+            self.downloader.maxConcurrentOperationCount = newValue
+        }
+        
+        get {
+            return self.downloader.maxConcurrentOperationCount
+        }
+    }
+    
+    var videoInfo: VideoInfo!
+    var avNumber: Int = 0
+    
+    lazy var downloader: VideoDownloader = {
+        let d = VideoDownloader()
+        d.delegate = self
+        return d
+    }()
+    
+    public init(av: Int) {
+        self.avNumber = av
+    }
+    
+    public func work() {
+        self.downloader.start()
+    }
+    
+    public func fetchVideoInfo() {
+        let videoInfo = VideoInfo(av: self.avNumber)
+        let (ret, _) = videoInfo.fetchInfo()
+        if (ret) {
+            print("Video \(videoInfo.title) Has Pages:")
+            videoInfo.pagesTitle.enumerated().forEach { (i) in
+                print("     \(i.offset): \(i.element)")
+            }
+            self.videoInfo = videoInfo
+        }
+    }
+    
+    public func downloader(_ downloader: VideoDownloader, didTriggerEvent: VideoDownloaderEvent) {
+        if (didTriggerEvent == .CanAddTask) {
+            if (self.downloadItem.count > 0) {
+                let item = self.downloadItem.removeFirst()
+                let page = VideoPageInfo(videoItem: self.videoInfo.videoPages[item.0])
+                page.fetchDownloadURL(quality: item.1)
+                downloader.addTask(url: page.downloadURL[0], videoItem: page.videoItem)
+            }
+        }
+    }
+}
