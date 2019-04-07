bfc62772d10724dd8ef8508bd1a8c05c23a5325d
eric3u <eric3u@outlook.com>
2019-04-08 00:00:43 +0800

    [Fix] Range

--- PuliPuli.playground/Contents.swift
+++ PuliPuli.playground/Contents.swift
@@ -1,18 +1,15 @@
 import Cocoa
-import CommonCrypto
 
 Env.checkAndCreateDownloadDocument()
 
 let pulipuli = PuliPuliDownloader(av: 47896059)
 pulipuli.fetchVideoInfo()
 var s: [(Int, VideoQuality)] = []
-for i in 0...1 {
+for i in 0..<1 {
     s.append((i, .q480P))
 }
 
 pulipuli.maxConcurrentOperationCount = 1
 pulipuli.downloadItem = s
 pulipuli.work()
-
-
 
